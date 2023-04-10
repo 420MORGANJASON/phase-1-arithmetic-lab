d719892f7aa8acf747d75e8f5a02e7fe2d184ebe
MORGANJASON <420morganjason@gmail.com>
2023-04-10 16:15:57 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+// multiply num1 and num2 to get 62
+num1 = 31
+num2 = 2
+multiply = num1 * num2
+
+// generate a random integer greater than 0
+import random
+
+random = random.randint(1, 100)
+// print(random)
+
+
+// calculate the remainder of dividing num3 by num4
+num3 = 16
+num4 = 3
+mod = num3 % num4
+
+// find the highest number in a set
+my_set = [7, 14, 20, 2, 6]
+Math.max= max(my_set)
